5953de2b87f520e7637cbd2c6a7dd62a0b0207a9
lluisalonsop <Lluis.AlonsoP@autonoma.cat>
2023-08-10 13:29:04 +0200

    Mongodb connection app

--- ServerCentralized/server.js
+++ ServerCentralized/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+const { generateKeyPairSync } = require('crypto'); // Importamos el módulo crypto
 
 const app = express();
 const port = 3000;
@@ -18,9 +19,23 @@ MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (
 
     // Ruta para generar y almacenar la clave SSH
     app.post('/generate-ssh-key', (req, res) => {
-        const { username, key } = req.body;
+        const { username } = req.body;
 
-        db.collection('keys').insertOne({ username, key }, (err, result) => {
+        // Generar la clave SSH aquí (usando el módulo 'crypto')
+        const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+            modulusLength: 4096,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem'
+            }
+        });
+
+        // Almacena las claves en la base de datos (aquí deberías agregar el código para hacerlo)
+        db.collection('keys').insertOne({ username, publicKey, privateKey }, (err, result) => {
             if (err) {
                 console.error('Error inserting key into database:', err);
                 res.status(500).json({ error: 'Internal server error' });
